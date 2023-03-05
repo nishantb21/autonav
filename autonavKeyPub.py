08751a22645063cf9aee5cb951ce28b10c14b2c0
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-03-05 16:36:25 -0700

    tested with rostopic echo

--- autonavKeyPub.py
+++ autonavKeyPub.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
 import rospy
-import os
-import keyboard
 from std_msgs.msg import Float32
+import termios
+import tty
+import sys
+from select import select
+
+#from https://github.com/ros-teleop/teleop_twist_keyboard/blob/master/teleop_twist_keyboard.py
+def getKey(settings, timeout):
+
+    tty.setraw(sys.stdin.fileno())
+    # sys.stdin.read() returns a string on Linux
+    rlist, _, _ = select([sys.stdin], [], [], timeout)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
 def autonavKeyPub():
+    #for keypresses
+    settings = termios.tcgetattr(sys.stdin)
+    keytimeout = 0.01
 
     topic = "servo_cmds"
     freq = 10 #frequency to publish at
@@ -17,12 +35,13 @@ def autonavKeyPub():
     while not rospy.is_shutdown():
 
         # if only left arrow: send 0.0
-        if keyboard.is_pressed("left") and not keyboard.is_pressed("right"):
+        key = getKey(settings=settings,timeout=keytimeout)
+        if key == 'a' and not key=='d':
             cmd = 0.0
 
 
         # if only right arrow: send 0.85
-        elif keyboard.is_pressed("right") and not keyboard.is_pressed("left"):
+        elif key == 'd' and not key=='a':
             cmd = 0.85
 
         #neither arrow key is pressed or both are pressed
