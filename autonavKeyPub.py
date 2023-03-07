9860dc58a553673e49afca44d464258af1d8192d
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-03-07 16:16:24 -0700

    worked around bug with key presses not registering

--- autonavKeyPub.py
+++ autonavKeyPub.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import rospy
 from std_msgs.msg import Float32
@@ -6,6 +6,10 @@ import termios
 import tty
 import sys
 from select import select
+
+STRAIGHT = 0.4
+LEFT = 0.0
+RIGHT = 0.85
 
 #from https://github.com/ros-teleop/teleop_twist_keyboard/blob/master/teleop_twist_keyboard.py
 def getKey(settings, timeout):
@@ -23,31 +27,34 @@ def getKey(settings, timeout):
 def autonavKeyPub():
     #for keypresses
     settings = termios.tcgetattr(sys.stdin)
-    keytimeout = 0.01
+    keytimeout = 0.25
 
     topic = "servo_cmds"
-    freq = 10 #frequency to publish at
+    freq = 5#frequency to publish at
 
     pub = rospy.Publisher(topic, Float32, queue_size=10)
     rospy.init_node('autonav_kb_pub', anonymous=True)
     rate = rospy.Rate(freq) # 10hz
     
+    #initialize cmd ot straight
+    cmd = STRAIGHT
     while not rospy.is_shutdown():
 
         # if only left arrow: send 0.0
         key = getKey(settings=settings,timeout=keytimeout)
-        if key == 'a' and not key=='d':
-            cmd = 0.0
+        if key == 'a':
+            cmd = LEFT
 
 
         # if only right arrow: send 0.85
-        elif key == 'd' and not key=='a':
-            cmd = 0.85
+        elif key == 'd':
+            cmd = RIGHT
 
-        #neither arrow key is pressed or both are pressed
-        else: 
-            cmd = 0.4
+        # s key for staight is pressed
+        elif key =='s': 
+            cmd = STRAIGHT
 
+        
         pub.publish(cmd)
         rate.sleep()
 
